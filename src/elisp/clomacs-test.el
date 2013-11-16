607bafe6d72a481a0d5a104c29c4c750b16ccc75
kostafey <kostafey@gmail.com>
2013-11-16 20:19:49 +0400

    Update elisp tests.

--- src/elisp/clomacs-test.el
+++ src/elisp/clomacs-test.el
@@ -1,19 +1,17 @@
-
 (ert-deftest clomacs-defun-test ()
        "Tests for `clomacs-defun'."
        (clomacs-defun summ-1 +)
        (should (equal (summ-1 2 3) "5"))
 
-       (clomacs-defun summ-2 + :number)
+       (clomacs-defun summ-2 + :return-type :number)
        (should (equal (summ-2 2 3) 5))
 
-       (clomacs-defun summ-3 + string-to-int)
+       (clomacs-defun summ-3 + :return-type string-to-int)
        (should (equal (summ-3 2 3) 5))
 
        (clomacs-defun str str)
-       (should (equal (str 2 "str") "2str"))       
+       (should (equal (str 2 "str") "2str"))
 
        (clomacs-defun get-property System/getProperty)
        (should (stringp (get-property "java.version")))
        )
-
