f13bf08318972a88e80f8dd1c6ce54c6d0dcaaaa
kostafey <kostafey@gmail.com>
2014-09-21 04:14:28 +0400

    Fix tests for namespace.

--- src/elisp/clomacs-test.el
+++ src/elisp/clomacs-test.el
@@ -1,22 +1,25 @@
 (ert-deftest clomacs-defun-test ()
-       "Tests for `clomacs-defun'."
-       (clomacs-defun summ-1 +)
-       (should (equal (summ-1 2 3) "5"))
+  "Tests for `clomacs-defun'."
+  (clomacs-defun summ-1 +)
+  (should (equal (summ-1 2 3) "5"))
 
-       (clomacs-defun summ-2 + :return-type :number)
-       (should (equal (summ-2 2 3) 5))
+  (clomacs-defun summ-2 + :return-type :number)
+  (should (equal (summ-2 2 3) 5))
 
-       (clomacs-defun summ-3 + :return-type string-to-int)
-       (should (equal (summ-3 2 3) 5))
+  (clomacs-defun summ-3 + :return-type string-to-int)
+  (should (equal (summ-3 2 3) 5))
 
-       (clomacs-defun str str)
-       (should (equal (str 2 "str") "2str"))
+  (clomacs-defun str str)
+  (should (equal (str 2 "str") "2str"))
 
-       (clomacs-defun get-property System/getProperty)
-       (should (stringp (get-property "java.version")))
+  (clomacs-defun get-property System/getProperty)
+  (should (stringp (get-property "java.version")))
 
-       (clomacs-defun get-property2 getProperty :namespace System)
-       (should (stringp (get-property2 "java.version"))))
+  (clomacs-defun make-clojure-list list :return-type :list)
+  (should (equal '(1 2 3) (make-clojure-list 1 2 3)))
+
+  (clomacs-defun make-clojure-vector vector :return-type :list)
+  (should (equal '[1 2 3] (make-clojure-vector 1 2 3))))
 
 (ert-deftest clomacs-integration-test ()
   "Integration test for `clomacs'."
--- test/clomacs/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clomacs.core-test
-  (:use clojure.test
-        clomacs.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
