f7bca89795d1041232f3f10287447c938b9ffc48
kostafey <kostafey@gmail.com>
2014-09-21 04:35:06 +0400

    Reorganize test files location.

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "https://github.com/kostafey/clomacs"
   :license {:name "General Public License 3"
             :url "http://www.gnu.org/licenses/gpl-3.0.html"}
+  :test-paths ["test/clj"]
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 ;; Used for clomacs self-testing.
                  [markdown-clj "0.9.47"]])
