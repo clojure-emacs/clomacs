8a47ab4580d5fb8558e2db81ae951770f90baedb
Bozhidar Batsov <bozhidar@batsov.com>
2016-03-05 17:44:15 +0200

    Update the dependency info

--- clomacs-pkg.el
+++ clomacs-pkg.el
@@ -1,3 +1,3 @@
-(define-package "clomacs" "0.0.1"
-                "Simplifies emacs lisp interaction with clojure"
-                '((nrepl "0.1.8")))
+(define-package "clomacs" "0.0.2"
+  "Simplifies emacs lisp interaction with clojure"
+  '((cider "0.11.0")))
